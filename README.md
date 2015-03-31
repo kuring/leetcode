eb5b6d2a28bd28516b8b32cc3a47aea73810cdfb
Kuring Lv <kuring.public@gmail.com>
2015-03-31 21:54:23 +0800

    add reverse words in a string

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ LeetCode C++ Solutions
 |[Find Minimum in Rotated Sorted Array II](https://oj.leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/)| [C++](./src/findMinimumInRotatedSortedArray/findMinimumInRotatedSortedArray.II.cpp)|Hard|yes|
 |[Find Minimum in Rotated Sorted Array](https://oj.leetcode.com/problems/find-minimum-in-rotated-sorted-array/)| [C++](./src/findMinimumInRotatedSortedArray/findMinimumInRotatedSortedArray.cpp)|Medium|yes|
 |[Maximum Product Subarray](https://oj.leetcode.com/problems/maximum-product-subarray/)| [C++](./src/maximumProductSubarray/maximumProductSubarray.cpp)|Medium|yes|
-|[Reverse Words in a String](https://oj.leetcode.com/problems/reverse-words-in-a-string/)| [C++](./src/reverseWordsInAString/reverseWordsInAString.cpp)|Medium|
+|[Reverse Words in a String](https://oj.leetcode.com/problems/reverse-words-in-a-string/)| [C++](./src/reverseWordsInAString/reverseWordsInAString.cpp)|Medium|yes|
 |[Evaluate Reverse Polish Notation](https://oj.leetcode.com/problems/evaluate-reverse-polish-notation/)| [C++](./src/evaluateReversePolishNotation/evaluateReversePolishNotation.cpp)|Medium|
 |[Max Points on a Line](https://oj.leetcode.com/problems/max-points-on-a-line/)| [C++](./src/maxPointsOnALine/maxPointsOnALine.cpp)|Hard|
 |[Sort List](https://oj.leetcode.com/problems/sort-list/)| [C++](./src/sortList/sortList.cpp)|Medium|
---
+++ src/reverseWordsInAString/reverse_words_in_a_string.cpp
@@ -0,0 +1,79 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+/**
+ * 本题的思路为：
+ * 1. 首先去除空格，去除开始和结束和字符串中重复出现的空格
+ * 2. 反转整个字符串
+ * 3. 反转字符串中的单个词
+ * 其中，第一步和第二步的操作可以合并为一个函数中处理，本程序为了简化没有做此处理
+ */
+class Solution {
+    public:
+        void reverseWords(string &s) {
+            reduce_space(s);
+            reverseWord(s, 0, s.length() - 1);
+
+            // reverse a word
+            int i = 0, j = 0;
+            while (i < (int)s.length())
+            {
+                if (s[i] == ' ')
+                {
+                    reverseWord(s, j, i - 1);
+                    j = i + 1;
+                }
+                i++;
+            }
+            if (i != j)
+            {
+                reverseWord(s, j, i - 1);
+            }
+        }
+
+        void reduce_space(string &s)
+        {
+            int j = 0, i = 0;
+            int length = s.length();
+            while (s[i] == ' ')
+            {
+                i++;
+            }
+            for (; i<length; i++, j++)
+            {
+                while (i < length - 1  && s[i] == ' ' && s[i + 1] == ' ')
+                {
+                    i++;
+                }
+                s[j] = s[i];
+            }
+            s.erase(j, i - j);
+            while (s[--j] == ' ')
+            {
+                s.erase(j);
+            }
+        }
+
+        void reverseWord(string &s, int left, int right)
+        {
+            while (left < right)
+            {
+                char tmp = s[left];
+                s[left] = s[right];
+                s[right] = tmp;
+                right--;
+                left++;
+            }
+        }
+};
+
+int main()
+{
+//    string s = " the  sky is blue ";
+    string s = "   a   b ";
+    Solution solution;
+    solution.reverseWords(s);
+    cout << s << endl;
+}
